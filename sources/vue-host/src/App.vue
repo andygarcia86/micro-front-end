3eae3bd7dbff3e948756e1accb96191abe4e1d59
Andy Garcia <andygarcia86@gmail.com>
2022-04-15 09:49:45 -0500

    Code refactoring

--- sources/vue-host/src/App.vue
+++ sources/vue-host/src/App.vue
@@ -1,48 +1,27 @@
 <template>
 <div class="mx-auto max-w-screen-xl">
-  <div style="grid-template-columns: 1fr 2fr 3fr;" class="grid gap-4 m-4">
-    <img
-      src="http://valleygrowlers.com/wp-content/uploads/2013/11/clean_line_circle_logo-300x300.jpg"
-    />
-    <div
-      class="text-2xl"
-    >
-      Come join us every day starting at noon… Valley Growlers would love to have you as a guest. Come have a pint or take a growler or crowler to go....
-    </div>
-    <Search />
+  <div>
+    user info
   </div>
-  <div style="grid-template-columns: 3fr 1fr;" class="grid gap-4 m-4">
+  <div class="grid grid-cols-3 gap-4">
     <div>
-      <img
-        src="http://valleygrowlers.com/wp-content/uploads/2013/11/ValleyGrowlers12.png"
-        style="object-fit: cover; width: 100%; max-height: 200px;"
-      />
-      <div
-        class="m-3 text-xl"
-      >
-        We also like 
-        <span
-          class="font-bold"
-        >
-          {{filteredTaps}}
-        </span>
+      Menu
+      <ul>
+        <li>Vue module</li>
+        <li>V2 module</li>
+      </ul>
+      <Search />  
+      <br>
+      What's In Your Growler
+      <Cart />
+    </div>
+    <div class="col-span-2">
+      <div class="m-3 text-xl">
+        We also like <span class="font-bold">{{filteredTaps}}</span>
       </div>
       <Taps />
     </div>
-    <div>
-      <div
-        class="font-bold text-2xl mb-3"
-      >
-        What's In Your Growler
-      </div>
-      <Cart />
-      <div
-        class="mt-3"
-      >
-        We have growlers and cans available if you don't have them already.
-      </div>
-    </div>
-  </div>
+  </div>  
 </div>
 </template>
 
